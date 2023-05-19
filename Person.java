811c3861a1afb29f54802946fadcf55214a6c96b
ALEH BIAHUN <stones2875@gmail.com>
2023-05-19 20:00:41 +0200

    Push №2 repository_new

--- Person.java
+++ Person.java
@@ -2,22 +2,17 @@ public class Person {
     public String fullName;                               //создали переменную "fullName" типа String
     public int age;                                       // создали переменную "age" типа int
 
-
     Person(String fullName, int age) {              //создали конструтор Person(fullName, age)
         this.fullName = fullName;
         this.age = age;
     }
-
     Person() {                                      //создали конструктор Person()
     }
-
     public void move() {
         System.out.println("Двигается " + this.fullName + "," + " ему " + this.age);
     }
-
     public void talk() {                                    //создали метод talk
         System.out.println("Говорит " + this.fullName);
     }
-
 }
 

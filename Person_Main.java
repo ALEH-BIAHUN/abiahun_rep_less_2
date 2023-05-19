5018031711c25ba177c1bf6999e45c21476c48db
ALEH BIAHUN <stones2875@gmail.com>
2023-05-19 19:58:23 +0200

    Push №2 repository

--- Person_Main.java
+++ Person_Main.java
@@ -4,14 +4,11 @@ public class Person_Main {
         Person person_1 = new Person("Иванов Иван Иванович", 35);                   //создали объект person_1
         Person person_2 = new Person();                   //создали объект person_2
 
-
-
         person_1.fullName = "Иванов Иван Иванович";      //присвоили значение переменной "fullName" типа String
         person_2.fullName = "Петров Петр Петрович";      //присвоили значение переменной "fullName" типа String
 
         person_1.age = 35;                               //присвоили значение переменной "age" типа int
         person_2.age = 44;                               //присвоили значение переменной "age" типа int
-
 
         person_1.move();                                 //вызываем метод move для объекта person_1
         person_2.move();                                 //вызываем метод move для объекта person_2
